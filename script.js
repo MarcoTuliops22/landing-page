3def50604dfea2f3a217db9e37b3c298b0b1f37e
Marco Túlio <marcotuliops22@gmail.com>
2025-12-11 12:21:16 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Animação suave ao rolar a página
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(sec => observer.observe(sec));
+
